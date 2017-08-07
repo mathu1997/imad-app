f50e6db99ca6548a993095da27ce620d572ce882
mathu1997 <30770898+mathu1997@users.noreply.github.com>
2017-08-07 21:48:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,7 @@
 console.log('Loaded!');
 alert("HI IAM MATHU. LET'S COME WITH ME");
+//move the image 
+var img = document.getElementById('lion');
+img.onclick = function(){
+    img.style.marginLeft = '100px';
+};
