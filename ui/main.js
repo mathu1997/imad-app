b312ed2fccb10c84da42a9a4097e08cd92b20e38
mathu1997 <30770898+mathu1997@users.noreply.github.com>
2017-08-07 22:01:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,11 +4,11 @@ alert("HI IAM MATHU. LET'S COME WITH ME");
 var img = document.getElementById('lion');
 var marginLeft =0;
 function moveRight (){
-    marginLeft = marginLeft + 10;
+    marginLeft = marginLeft + 5;
     img.style.marginLeft = marginLeft + 'px';
     
 }
 img.onclick = function(){
-    var interval = setInterval(moveRight,100);
+    var interval = setInterval(moveRight,50);
     
 };
