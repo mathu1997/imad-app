8c4fd4c28322e8b60fb32b9720a894f291735889
mathu1997 <30770898+mathu1997@users.noreply.github.com>
2017-08-07 21:59:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,13 @@ console.log('Loaded!');
 alert("HI IAM MATHU. LET'S COME WITH ME");
 //move the image 
 var img = document.getElementById('lion');
+var marginLeft =0;
+function moveRight (){
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+    
+}
 img.onclick = function(){
-    img.style.marginLeft = '100px';
+    var interval = setInterval(moveleft,100);
+    
 };
