f81e3c1033c9f9f9c1f439c9c16f25fa234ac6b1
mathu1997 <30770898+mathu1997@users.noreply.github.com>
2017-08-25 19:21:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto =  require('crypto');
 
 var config = {
     user:  'mathu1997mm',
@@ -17,6 +18,17 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+function hash (input) {
+    
+    var hashed = crypto.pbkdf25ync(input, salt, 1000, 512, 'sha512');
+    return hashed.toString('hex');
+}
+app.get('/hash/:input', function(req, res) {
+    var hashedString = hash(req,params,input);
+    res.send(hashedString);
+});
+
 
 var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
