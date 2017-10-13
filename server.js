5e7be3abbf2de245bdc28d7a84f8cd673b3f9d4b
mathu1997 <30770898+mathu1997@users.noreply.github.com>
2017-10-13 15:05:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,90 +1,11 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var Pool = require('pg').Pool;
-var crypto =  require('crypto');
-var bodyParser =  require('body-parser');
-var config = {
-    user:  'mathu1997mm',
-    database:	'mathu1997mm',
-    host: 'db.imad.hasura-app.io',
-    port:'5432',
-    password: 'db-mathu1997mm-35450'
-};
-
-var app = express();
-app.use(morgan('combined'));
-app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-function hash (input, salt) {
-    
-    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-    return[".pbkdf2","10000", salt, hashed.toString('hex')].join('$');
-    
-    
-}
-app.get('/hash/:input', function(req, res) {
-    var hashedString = hash(req.params.input, 'this-is-some-random-string');
-    res.send(hashedString);
-});
-
-app.post('/create-user', function(req, res) {
-   var username = req.body.username;
-   var password = req.body.password;    
-   var salt = crypto.randomBytes(128).toString('hex');
-   var dbString = hash(password, salt); 
-   pool.query('INSERT INFO "user"(username, password) VALUES ($1, $2)', [username, dbString], function(err, request){
-         if (err) {
-            res.status(500).send(err.toString());
-        } else {
-            res.send('user successfuly created ' + username);
-        }
-       
-   });
-});
-
-app.post('/login', function(req, res) {
-     var username = req.body.username;
-     var password = req.body.password;
-     
-   pool.query('SELECT * FROM "user" WHERE username =$1', [username], function(err, request){
-       if (err) {
-            res.status(500).send(err.toString());
-        } else {
-            if(result .rows.length===0) {
-                res.send(403).send('username/passwaed is invalid');
-            } else {
-                var dbString = result.rows[0].password;
-                var salt = dbString.split('$')[2];
-                var hashedPassword = hash(password, salt);
-                if (hashedPassword === dbString) {
-                    res.send('cridentials correct')
-                } else {
-                  res.send(403).send('username/passwaed is invalid');
-                }
-            }
-        }
-   });
-});
-
-var pool = new Pool(config);
-app.get('/test-db', function (req, res) {
-    //make a sselect request
-    //return a response with the result
-    pool.query('SELECT * FROM test', function(err, result) {
-        if (err) {
-            res.status(500).send(err.toString());
-        } else {
-            
-            res.send(JSON.stringify(result));
-        }
-    });
-});
-    
 app.get('/ui/mathu-0ne.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'mathu-0ne.html'));
 });
